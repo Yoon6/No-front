ee64c36ded6ce5e4f65ea59f00d41acd53ffc8b7
maxyoon <yoonda5898@gmail.com>
2022-04-29 16:40:30 +0900

    Feat: make main page and join page

--- src/main/java/com/pathhack/nofront/controller/HomeController.java
+++ src/main/java/com/pathhack/nofront/controller/HomeController.java
@@ -1,7 +1,18 @@
 package com.pathhack.nofront.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+
+    @GetMapping("/")
+    public String getMainPage() {
+        return "index";
+    }
+
+    @GetMapping("/object")
+    public String getObjectPage() {
+        return "object";
+    }
 }
