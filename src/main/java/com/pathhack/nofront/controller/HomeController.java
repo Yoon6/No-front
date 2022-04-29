ba5b24d60c3c0c3535e7c72611807c59b4412e5b
maxyoon <yoonda5898@gmail.com>
2022-04-29 20:28:42 +0900

    Feat: make socket server

--- src/main/java/com/pathhack/nofront/controller/HomeController.java
+++ src/main/java/com/pathhack/nofront/controller/HomeController.java
@@ -1,10 +1,17 @@
 package com.pathhack.nofront.controller;
 
+import com.pathhack.nofront.service.HomeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HomeController {
+
+    @Autowired
+    private HomeService homeService;
 
     @GetMapping("/")
     public String getMainPage() {
@@ -14,5 +21,17 @@ public class HomeController {
     @GetMapping("/object")
     public String getObjectPage() {
         return "object";
+    }
+
+    @PostMapping("/join")
+    public String postNickname(@RequestParam("nickname") String nickname) {
+
+        try {
+            homeService.makeSocketConnection(nickname);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return "redirect:/object";
     }
 }
--- src/main/java/com/pathhack/nofront/service/HomeService.java
+++ src/main/java/com/pathhack/nofront/service/HomeService.java
@@ -1,7 +1,22 @@
 package com.pathhack.nofront.service;
 
+import com.pathhack.nofront.WebSocketUtil;
+import org.java_websocket.drafts.Draft_6455;
 import org.springframework.stereotype.Service;
+
+import java.net.URI;
+
 
 @Service
 public class HomeService {
+    public void makeSocketConnection(String nickname) throws Exception {
+
+        System.out.println("nickname : " + nickname);
+
+        URI uri = new URI("ws://localhost:8080/room");
+        WebSocketUtil util = new WebSocketUtil(uri, new Draft_6455());
+
+
+        util.connectBlocking();
+    }
 }
