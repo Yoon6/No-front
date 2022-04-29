f29f1b8854181348585a0c9c33126305faaf618e
maxyoon <yoonda5898@gmail.com>
2022-04-29 15:16:55 +0900

    change dependency

---
+++ src/main/java/com/pathhack/nofront/NofrontApplication.java
@@ -0,0 +1,13 @@
+package com.pathhack.nofront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NofrontApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NofrontApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pathhack/nofront/NofrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pathhack.nofront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NofrontApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
