1a4c9e35bf2e790a769c87363e33e4a1097becc3
maxyoon <yoonda5898@gmail.com>
2022-04-30 03:09:26 +0900

    Feat: Add css at main page

--- src/main/java/com/pathhack/nofront/controller/HomeController.java
+++ src/main/java/com/pathhack/nofront/controller/HomeController.java
@@ -3,6 +3,7 @@ package com.pathhack.nofront.controller;
 import com.pathhack.nofront.service.HomeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,15 +20,25 @@ public class HomeController {
     }
 
     @GetMapping("/object")
-    public String getObjectPage() {
+    public String getObjectPage(@RequestParam("nickname") String nickname, Model model) {
+
+        model.addAttribute("nickname", nickname);
+
         return "object";
+    }
+
+    @GetMapping("/game")
+    public String getGamePage(@RequestParam("nickname") String nickname, Model model) {
+        model.addAttribute("nickname", nickname);
+
+        return "game";
     }
 
     @PostMapping("/join")
     public String postNickname(@RequestParam("nickname") String nickname) {
 
         try {
-            homeService.makeSocketConnection();
+//            homeService.makeSocketConnection();
             homeService.saveUser(nickname);
         } catch (Exception e) {
             e.printStackTrace();
